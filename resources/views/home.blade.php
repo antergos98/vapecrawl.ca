d7c061f62d9991dda61e038b2751500b3f0dcf5e
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2020-06-24 00:31:38 -0400

    WIP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,12 @@
         <div class="mb-6">
             <h1 class="text-2xl mb-6 md:text-3xl">Welcome on Vapecrawl</h1>
             <p>
-                Vapecrawl is a simple search engine that let you search through our database of 15 000 products sold by
+                Vapecrawl is a simple search engine that let you search through our database of 17 000+ products sold by
                 several Canadian vendors. Vapecrawl is an attempt to scratch my own itch, but I thought it would
                 be great to share it with everyone. <br><br> We know it can be a pain in the ass to spend hours on Google to find
                 who
                 carries the product you are looking for for the cheapest price possible. Whether you are looking for a new
-                mod, tank or whatever else related to vaping, we got you covered! Please note that we do not own or sell any of the products listed on this site.
+                mod, tank or whatever else related to vaping, we got you covered! Please note that we do not own or sell any of the products listed on this site. Vapecrawl is intended to be used by adults ONLY!
             </p>
         </div>
         <form action="/search" method="GET">
