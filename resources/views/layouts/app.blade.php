985fc8ed6afaf3fcaaf55900406fa6388baa1a68
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2020-07-27 19:18:53 -0400

    WIP

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,23 +32,7 @@
 @include('shared._import_alert')
 <div class="flex flex-col min-h-screen justify-between">
     <div>
-        <nav class="mt-6 mb-12 md:mb-24">
-            <div class="max-w-6xl mx-auto px-6 xl:px-0">
-                <div class="flex flex-col items-center md:flex-row">
-                    <div class="md:mr-12 mb-3 md:mb-0">
-                        <a href="/" class="text-3xl font-bold ">Vapecrawl</a>
-                    </div>
-                    <div>
-                        <ul class="flex font-bold space-x-6">
-                            <li><a class="hover:underline" href="/search">Search</a></li>
-                            <li><a class="hover:underline" href="/contact">Contact us</a></li>
-                            <li><a class="hover:underline" href="/faq">FAQ</a></li>
-                            <li><a class="hover:underline" href="/vendors">Vendors</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
+       @include('shared._header')
         <main class="mb-8">
             <div class="max-w-6xl mx-auto px-6 xl:px-0 mb-6">
                 @yield('content')
---
+++ resources/views/shared/_header.blade.php
@@ -0,0 +1,17 @@
+<nav class="mt-6 mb-12 md:mb-24">
+    <div class="max-w-6xl mx-auto px-6 xl:px-0">
+        <div class="flex flex-col items-center md:flex-row">
+            <div class="md:mr-12 mb-3 md:mb-0">
+                <a href="/" class="text-3xl font-bold ">Vapecrawl</a>
+            </div>
+            <div>
+                <ul class="flex font-bold space-x-6">
+                    <li><a class="hover:underline" href="/search">Search</a></li>
+                    <li><a class="hover:underline" href="/contact">Contact us</a></li>
+                    <li><a class="hover:underline" href="/faq">FAQ</a></li>
+                    <li><a class="hover:underline" href="/vendors">Vendors</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
