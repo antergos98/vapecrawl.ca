24e81b13d9a603c4b7fd22508d6b1c4e660993a0
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2020-08-14 22:18:58 -0400

    Add WcImporter

---
+++ app/Importer/WCImporter.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Importer;
+
+use App\Product;
+use App\Vendor;
+use Illuminate\Support\Facades\Http;
+
+class WCImporter implements ImporterInterface
+{
+    private Vendor $vendor;
+
+    public function __construct(Vendor $vendor)
+    {
+        $this->vendor = $vendor;
+    }
+
+    public function import(): void
+    {
+        $page = 1;
+        $products = [];
+
+        while (true) {
+            $apiUrl = $this->vendor->url . "wp-json/wc/store/products?per_page=100&page={$page}";
+            $results = Http::get($apiUrl)->json();
+
+            if (count($results) === 0) {
+                break;
+            }
+
+            foreach ($results as $result) {
+                $normalized = $this->getNormalizedResult($result);
+                if ($normalized['price'] !== "0" && $normalized['image']) {
+                    $products[] = $normalized;
+                }
+            }
+
+            $page++;
+        }
+
+        collect($products)->chunk(100)->each(fn($products) => Product::insert($products->all()));
+    }
+
+    protected function getNormalizedResult(array $result): array
+    {
+        return [
+            'name' => $result['name'],
+            'price' => $result['prices']['sale_price'],
+            'image' => $result['images'][0]['src'] ?? null,
+            'in_stock' => $result['is_in_stock'],
+            'url' => $result['permalink'],
+            'real_id' => (int)$result['id'],
+            'vendor_id' => $this->vendor->id
+        ];
+    }
+}
