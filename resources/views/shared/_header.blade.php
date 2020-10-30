918b71cf4003c7e79f0785d0f7721e74d1f42844
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2020-10-29 22:12:03 -0400

    WIP

--- resources/views/shared/_header.blade.php
+++ resources/views/shared/_header.blade.php
@@ -1,14 +1,14 @@
 <nav>
-    <div class="max-w-screen-xl mx-auto px-6 xl:px-0 text-white">
+    <div class="max-w-screen-xl mx-auto px-6 xl:px-0">
         <div class="flex justify-between flex-col sm:flex-row items-center">
-            <div>
+            <div class="mb-3 sm:mb-0">
                 <a href="/">
                     @include('shared._logo_svg')
                 </a>
             </div>
             <div>
-                <ul class="flex font-bold space-x-8 text-lg">
-                    <li><a class="hover:underline text-white" href="/search">Search</a></li>
+                <ul class="flex font-bold space-x-6">
+                    <li><a class="hover:underline" href="/search">Search</a></li>
                     <li><a class="hover:underline" href="/contact">Contact us</a></li>
                     <li><a class="hover:underline" href="/vendors">Vendors</a></li>
                 </ul>
