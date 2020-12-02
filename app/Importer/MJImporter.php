91619178585372d229f4f365d02d8d82efc10057
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2020-12-01 19:07:03 -0500

    WIP

---
+++ app/Importer/MJImporter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Importer;
+
+use App\Product;
+use App\Vendor;
+use Goutte\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+class MJImporter implements ImporterInterface
+{
+    private Vendor $vendor;
+    private Client $client;
+    private array $products = [];
+
+    public function __construct(Vendor $vendor)
+    {
+        $this->vendor = $vendor;
+        $this->client = new Client();
+    }
+
+    public function import(): void
+    {
+        $url = $this->vendor->url . "products";
+        $crawler = $this->client->request('GET', $url);
+
+        $crawler->filter('.catalog-item')->each(function (Crawler $crawler) {
+            $name = $crawler->filter('.product-title')->text();
+            $image = $crawler->filter('.product-gallery')->count()
+                ? $crawler->filter('.product-gallery img')->attr('src')
+                : null;
+            $price = $crawler->filter('.product-price span')->text();
+            $price = (float)str_replace('$', '', $price);
+            $price *= 100;
+            $url = $crawler->filter('a.colorbox-node')->attr('href');
+
+            $this->products[] = [
+                'name' => $name,
+                'image' => $image,
+                'price' => $price,
+                'url' => $url,
+                'vendor_id' => $this->vendor->id,
+                'in_stock' => true
+            ];
+        });
+
+        Product::insert($this->products);
+    }
+}
