1d1f99cdd91581e7fc66742fab9dd02366f16145
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2020-12-08 11:45:15 -0500

    Fuck analytics

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,9 +10,7 @@
 
     <meta name="description"
           content="Vapecrawl is a vape search engine that lets you search across dozens of Canadian vendors to find the products you are looking for at the best price.">
-
-    @includeWhen(app()->environment('production'), 'shared._analytics')
-
+    
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
